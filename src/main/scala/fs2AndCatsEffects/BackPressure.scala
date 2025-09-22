5f4b3d63032b322e7a11bc4135670f3962db7544
Narawut Kongduang <narawut.kongduang@gmail.com>
2025-09-22 16:04:16 +0200

    add comment for learning

--- src/main/scala/fs2AndCatsEffects/BackPressure.scala
+++ src/main/scala/fs2AndCatsEffects/BackPressure.scala
@@ -11,7 +11,7 @@ object BackPressure extends IOApp.Simple {
     //when using a List (1 to 1_000_000).toList) creates instantly in memory for all elements!
     Stream.range(1, 1_000_000) //  --> basically lazy load ony by one and only when needed
       .filter(_ % 2 == 0) // just for the understanding for "only when needed" part
-      .evalMap(item =>
+      .evalMap(item => // Can perform side effects, async operations, error handling + Effects are executed one at a time in order
         IO.sleep(100.millis) >> // simulate network call
         IO(slowProcess(item * 10))) // multiply by 10 to show it's pure side effects and no computation
       .compile.drain // THROWS AWAY all values, keeps only side effects!
