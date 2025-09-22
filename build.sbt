90f7bc1971e4a2d3bcbdb4a932280f9e02e78d5e
Narawut Kongduang <narawut.kongduang@gmail.com>
2025-09-22 22:29:28 +0200

    add first unit tests

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ lazy val root = (project in file("."))
   .settings(
     name := "first-scala",
     libraryDependencies += "co.fs2" %% "fs2-core" % "3.12.0",
-    libraryDependencies += "co.fs2" %% "fs2-io" % "3.12.0"
+    libraryDependencies += "co.fs2" %% "fs2-io" % "3.12.0",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.17" % Test
   )
 
---
+++ src/test/scala/IntIteratorTest.scala
@@ -0,0 +1,33 @@
+import basics.IntIterator
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class IntIteratorTest extends AnyFunSuite with Matchers {
+
+  test("IntIterator_shouldIterate0ToN-1_equalList") {
+    val iterator = new IntIterator(5)
+    val result = scala.collection.mutable.ListBuffer[Int]()
+    while (iterator.hasNext) {
+      result += iterator.next()
+    }
+    result.toList shouldEqual List(0, 1, 2, 3, 4)
+  }
+
+  test("IntIterator_setTo0_emptyList") {
+    val iterator = new IntIterator(0)
+    iterator.hasNext shouldBe false
+    val result = scala.collection.mutable.ListBuffer[Int]()
+    while (iterator.hasNext) {
+      result += iterator.next()
+    }
+    result.toList shouldEqual List.empty
+  }
+
+  test("IntIterator_setTo1_oneElementInList") {
+    val iterator = new IntIterator(1)
+    iterator.hasNext shouldBe true
+    iterator.next() shouldEqual 0
+    iterator.hasNext shouldBe false
+  }
+  
+}
