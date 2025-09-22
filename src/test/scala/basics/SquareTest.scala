19cad988b17e11748936dbcbb5e32e280660a768
Narawut Kongduang <narawut.kongduang@gmail.com>
2025-09-22 23:00:32 +0200

    add unit test for WeightConverter

--- src/test/scala/basics/SquareTest.scala
+++ src/test/scala/basics/SquareTest.scala
@@ -3,13 +3,20 @@ package basics
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class SquareTest extends AnyFunSuite with Matchers {
-  test("multipleNumbers_squareRooting_showResult") {
-    val sqrtFunction = basics.createSquareRoot()
+class WeightConverterTest extends AnyFunSuite with Matchers {
+  test("convertInLb_KGconvertToLB_showCorrectLB") {
+    val converter = WeightConverter(Seq(50.0, 100.0))
 
-    sqrtFunction(25) shouldBe 5.0
-    sqrtFunction(16) shouldBe 4.0
-    sqrtFunction(9) shouldBe 3.0
-    sqrtFunction(77) shouldBe 8.774964387392123
+    // couldn't figure out how to handle floating-point precision issue
+    // found this: https://stackoverflow.com/questions/11106886/scala-doubles-and-precision
+    // but it needs to modify production code, I don't like that
+    converter.convertInLb shouldBe Seq(110.00000000000001, 220.00000000000003)
+  }
+
+  test("afterWeightLoss_subtractWeightLoss_showNewWeight") {
+    val converter = WeightConverter(Seq(80.0))
+    val afterLoss = converter.afterWeightLoss(5.0)
+
+    afterLoss.weightInKg shouldBe Seq(75.0)
   }
 }
